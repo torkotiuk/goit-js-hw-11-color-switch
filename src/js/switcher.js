0f4e005341a78a9f4c710c5067cff1f412dbca9d
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-03-02 01:02:48 +0100

    finnished task

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,10 +1,31 @@
-import colors from './data/colors.js';
-const body = document.querySelector('body');
+import colors from './data/colors';
 
-const startBtn = document.querySelector('button[data-action="start"]');
-const stopBtn = document.querySelector('button[data-action="stop"]');
-
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
 // Для генерации случайного числа
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+let timerId = null;
+
+function handlerStartBtn() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+  refs.startBtn.disabled = true;
+  console.log('setInterval started!-!-!-!');
+}
+
+function handlerStopBtn() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  console.log('setInterval stopped!');
+}
+
+refs.startBtn.addEventListener('click', handlerStartBtn);
+
+refs.stopBtn.addEventListener('click', handlerStopBtn);
